414b933aa6b238d8a28da24001cab135e8aa8187
Irina <grisha9419@yandex.ru>
2023-05-07 01:23:29 +0300

    add final changes

--- work/Program.cs
+++ work/Program.cs
@@ -1,16 +1,18 @@
-﻿string[] array1={"Hello","2","world","Kazan"};
+﻿string[] array1={"Hello","2","world","Kazan","354"};
 int n=3;
 string[] array2 = new string[array1.Length] ;
 int i=0;
-for( int j=0; j<=array1.Length; j++)
+for( int j=0; j< array1.Length; j++)
 {
-    if (array1[j].Length <= n)
+    if(array1[j].Length <= n)
     {
         array2[i]=array1[j];
+        Console.Write($"{array2[i]}, ");
         i++;
+       
     }
 }
-if (i>0)
-Console.WriteLine(array2);
-else
+if (i==0)
+{
 Console.WriteLine("Элементов удовлетворяющих условию не найдено");
+}
